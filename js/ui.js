1223931ecad8f024781227423a79adaf2846c452
Alvaro <6666alvaro666@gmail.com>
2020-05-04 20:06:14 -0500

    create dynamic options in select

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,26 @@
 class UI {
+  constructor() {
+    this.init();
+  }
+  init() {
+    this.builOptions();
+  }
+  //insert fields for options
+  builOptions() {
+    api.getCoinsAPI().then((coins) => {
+      //get selector
+      const select = document.querySelector("#criptomoneda");
+
+      for (const [key, value] of Object.entries(coins.Data)) {
+        // add Symbol and name in options
+        const option = document.createElement("option");
+        option.value = value.Symbol;
+        option.appendChild(document.createTextNode(value.CoinName));
+        select.appendChild(option);
+      }
+    });
+  }
+  //show messages
   ShowMessage(message, classes, container) {
     const div = document.createElement("div");
     div.className = classes;
